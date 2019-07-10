096ef8950b99da5f8e4e1204c46bccf0a8fac52b
klkl-you <52692483+klkl-you@users.noreply.github.com>
2019-07-10 16:01:14 +0800

    Update layout.js

--- src/layout.js
+++ src/layout.js
@@ -41,7 +41,11 @@ export default class index extends React.Component {
   </Card>
   <Card style={{ width: 300}}>
     <p>Card content</p>
+<<<<<<< HEAD
     <p>Card content</p>
+=======
+    <p>黄金</p>
+>>>>>>> parent of d9b912a... Update layout.js
     <p>dsfl</p>
   </Card>
   
